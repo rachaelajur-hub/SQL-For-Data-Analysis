b7c245f3e4cca30b1202b9039031b6677352d4fd
rachaelajur-hub <rachaelajur@gmail.com>
2025-11-17 17:00:04 +0300

    Add files via upload

---
+++ Basic SQL Queries.sql
@@ -0,0 +1,35 @@
+INSERT INTO students (student_id, gender, age, course) 
+VALUES
+
+ (1, "Jeniffer Akoi"," Female","20","Data Analysis")
+ (2,"Esther Gichuna","Female","19","Digital Marketing")
+ (3,"Benjamin Njogu"," Male","23","Data Analysis")
+ (4,"Tyron Areva","Male","21","Data Analysis")
+ (5,"Purity Wangui","Female","Cyber Security")
+ ;
+ 
+ SELECT * FROM students;
+-- Select all Columns
+SELECT *
+FROM students;
+
+-- SELECT Full name and courses
+SELECT*
+FROM students;
+
+-- SELECT Retrieve a list of unique courses
+SELECT*
+FROM students;
+
+-- RENAME Full_name as'Full Name'
+SELECT*
+FROM Full_name;
+
+-- Concatenate Full_name and course as 'Student_Details'
+SELECT*
+FROM students;
+
+
+
+
+
